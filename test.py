43cfe85bcca796e20266b6e787418ffec4d7254d
EthanChong777 <ethanchong777@gmail.com>
2023-07-27 14:55:36 -0700

    Facial Recognition 1

--- test.py
+++ test.py
@@ -1,37 +1,66 @@
 import cv2
+import matplotlib.pyplot as plt
 import sys
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+img = cv2.imread("/Users/ethanc/Library/Photos/Libraries/Syndication.photoslibrary/scopes/syndication/resources/derivatives/4/49B1B64A-0BC8-4E9E-B829-79F167613435_1_102_o.jpeg")
+img.shape
+(4000, 2667, 3)
+
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_image.shape
+(4000, 2667)
+
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+) 
+
+face = face_classifier.detectMultiScale(
+    gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+)
+
+for (x, y, w, h) in face:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+plt.figure(figsize=(20,10))
+plt.imshow(img_rgb)
+plt.axis('off')
+
+ace_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 
 video_capture = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
-
-    # Draw a rectangle around the faces
+def detect_bounding_box(vid):
+    gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    return faces
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+while True:
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    result, video_frame = video_capture.read()  # read frames from the video
+    if result is False:
+        break  # terminate the loop if the frame is not read successfully
+
+    faces = detect_bounding_box(
+        video_frame
+    )  # apply the function we created to the video frame
+
+    cv2.imshow(
+        "My Face Detection Project", video_frame
+    )  # display the processed frame in a window named "My Face Detection Project"
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-# When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
+
+
 
 
