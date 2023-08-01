d9c69c693af4962f14c753055f64823e55f63bb9
Rhyan Shah <rhyan.shah@gmail.com>
2023-08-01 10:27:08 -0700

    Sound Test

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import cv2
 import cvlib as cv
+import time
 
 from cvlib.object_detection import draw_bbox
 from gtts import gTTS
 from playsound import playsound
 from distance import Distance_finder
+
+global dist
 
 video = cv2.VideoCapture(0)
 labels = []
@@ -25,13 +28,24 @@ while True:
     for i in range(0,len(bbox)):
         x = slice(i,i+1)
         bbox_split = bbox[x]
-        bbox_index = bbox_split[0]
-        print(bbox_split)
-        
+        bbox_index = bbox_split[0]                                              
         for object in frame:
             bbox_x = bbox_index[2]
             bbox_y = bbox_index[3]
-            print(Distance_finder(1171.58, float(bbox_x), float(bbox_y)), 'inches.')
+            dist = Distance_finder(1171.58, float(bbox_y), float(bbox_x))
+            print(dist)
+            
+            #Link if it gets deleted: "C:\Users\Rhyan Shah\Downloads\timer_beep.mp3"
+            
+            if dist <= 12:
+                playsound('C:/Users/Rhyan Shah/Downloads/timer_beep.mp3')
+                time.sleep(1)
+            elif 12 < dist <= 36:
+                playsound("C:/Users/Rhyan Shah/Downloads/timer_beep.mp3")
+                time.sleep(5)
+            elif 36 < dist:
+                playsound("C:/Users/Rhyan Shah/Downloads/timer_beep.mp3")
+                time.sleep(10)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
