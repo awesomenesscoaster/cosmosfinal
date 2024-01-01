cf096dae84845c46bea7513743540f855fa39093
Rhyan Shah <rhyan.shah@gmail.com>
2024-01-01 02:02:01 +0100

    Fully finished program

--- Facial_Recognition/Machine_Learning.py
+++ Facial_Recognition/Machine_Learning.py
@@ -11,6 +11,11 @@ from facial_recognition import img_numpy_array_list
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+userCountL = []
+for i in integer_img_conversion:
+    if i not in userCountL:
+        userCountL.append(i)
+
 x_data = img_numpy_array_list
 y_data = np.array(integer_img_conversion)
 x_data = img_numpy_array_list.reshape(-1, 200, 200, 1)
@@ -20,28 +25,34 @@ x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.
 
 model_cnn = keras.Sequential()
 
-num_filters = 8
-filter_size = 3
-pool_size = 2
+num_filters = 150
+filter_size = 5
+pool_size = 4
 
 model_cnn.add( keras.layers.Conv2D(num_filters, filter_size, input_shape=(200,200,1)) )
 model_cnn.add( keras.layers.MaxPooling2D(pool_size=pool_size) )
 model_cnn.add( keras.layers.Flatten() )
-model_cnn.add( keras.layers.Dense(2, activation='softmax') )
+model_cnn.add( keras.layers.Dense(8, activation='softmax') )
+model_cnn.add( keras.layers.Dense(1)) #len(userCountL)
 print('Complete!')
     
 model_cnn.compile(
-'adam',
+optimizer = 'adam',
 loss='categorical_crossentropy',
 metrics=['accuracy'],
 )
-    
+
+#model_cnn.summary()
+
+model_cnn.fit(x_train,y_train,epochs=2,validation_data=(x_test,y_test))
+
 # model_cnn.fit(
 # x = x_train,
 # y=keras.utils.to_categorical(y_train),
-# epochs=2,
-# batch_size=10
+# epochs=10,
+# batch_size=len(y_train)
 # )
+print('Complete 2.0')
 
-# score = model_cnn.evaluate(x_test, y_test)
-# print(score)
+test_lost, test_acc = model_cnn.evaluate(x_test, y_test)
+print(test_acc)
--- Facial_Recognition/facial_recognition.py
+++ Facial_Recognition/facial_recognition.py
@@ -2,23 +2,22 @@ import cv2
 import os
 import numpy as np
 
-import time
-
 from tensorflow import keras
-from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import img_to_array
+from sklearn.model_selection import train_test_split
 
-global picture_files
-global dir_files_cropped
-global integer_img_conversion
-global img_numpy_array_list
+# global picture_files
+# global dir_files_cropped
+# global img_numpy_array_list
+# global integer_img_conversion
+# global model_cnn
 
 scanning_process = False
 guessing_process = False
 
 user_name = input("What is your name? ")
 
-folder_path =  folder_path = os.path.join('C:/Users/Rhyan Shah/Documents/GitHub/cosmosfinal/Facial_Recognition/Datasets', user_name)
+folder_path = os.path.join('C:/Users/Rhyan Shah/Documents/GitHub/cosmosfinal/Facial_Recognition/Datasets', user_name)
 # if user_name not in names:
 #     scanning_process = True
 #     names.append(user_name)
@@ -37,6 +36,8 @@ else:
     guessing_process = True
     print('Welcome back:', user_name)
 
+model_cnn = keras.Sequential()
+
 if scanning_process == True:
     cam = cv2.VideoCapture(0)
     cv2.namedWindow("Facial Recognition")
@@ -45,7 +46,6 @@ if scanning_process == True:
     
     while True:
         ret, frame = cam.read()
-        
         if not ret:
             print("Failed.")
             
@@ -63,7 +63,7 @@ if scanning_process == True:
         cv2.imshow("Facial Recognition", frame)
         
         k = cv2.waitKey(1)  
-        
+        fo = os.chdir(folder_path)
         #Escape Key
         if k%256 == 27:
             print("Escape")
@@ -71,7 +71,10 @@ if scanning_process == True:
         
         #Space Bar
         elif k%256 == 32:
-            face_screenshot(15)
+            img_name = "opencv_frame_{}.png".format(img_counter)
+            cv2.imwrite(img_name,frame)
+            print('Screenshot Taken')
+            img_counter += 1
             
     cam.release()
     cv2.destroyAllWindows()
@@ -101,9 +104,6 @@ if scanning_process == True:
                 img_numpy_array_list.append(img_numpy_array)
     
     img_numpy_array_list = np.array(img_numpy_array_list)
-    print(picture_files)
-    print(dir_files_cropped)
-    
     name_counter = 0
     
     #FIgure this SHIT out so that there is actually x number of data outputted; rn there is only x-1 data shown
@@ -116,5 +116,104 @@ if scanning_process == True:
             name_counter += 1
     integer_img_conversion.append(name_counter)
     
-    print(integer_img_conversion)
+    userCountL = []
+    for i in integer_img_conversion:
+        if i not in userCountL:
+            userCountL.append(i)
     
+    x_data = img_numpy_array_list
+    y_data = np.array(integer_img_conversion)
+    x_data = img_numpy_array_list.reshape(-1, 200, 200, 1)
+    print(x_data.shape)
+    
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.20, random_state=0)
+    
+    num_filters = 150
+    filter_size = 5
+    pool_size = 4
+    
+    model_cnn.add( keras.layers.Conv2D(num_filters, filter_size, input_shape=(200,200,1)) )
+    model_cnn.add( keras.layers.MaxPooling2D(pool_size=pool_size) )
+    model_cnn.add( keras.layers.Flatten() )
+    model_cnn.add( keras.layers.Dense(8, activation='softmax') )
+    model_cnn.add( keras.layers.Dense(1)) #len(userCountL)
+    print('Complete!')
+    
+    model_cnn.compile(
+    optimizer = 'adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy'],
+    )
+    
+    model_cnn.fit(x_train,y_train,epochs=2,validation_data=(x_test,y_test))
+    
+    print('Complete 2.0')
+
+    test_lost, test_acc = model_cnn.evaluate(x_test, y_test)
+    print(test_acc)
+
+if guessing_process == True:
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow('Facial Recognition')
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print('Failed')
+        k = cv2.waitKey(1)
+        if k%256 == 32:
+            fo = os.chdir('C:/Users/Rhyan Shah/Documents/GitHub/cosmosfinal/Facial_Recognition/TestingData')
+            img_name = 'opencv_frame.png'
+            cv2.imwrite(img_name,frame)
+            print('Screenshot taken')
+        if k%256 == 27:
+            print('Escape')
+            break
+        cv2.imshow("Facial Recognition", frame)
+    cam.release()
+    cv2.destroyAllWindows()
+    
+    test_path = 'C:/Users/Rhyan Shah/Documents/GitHub/cosmosfinal/Facial_Recognition/TestingData'
+    img_name = 'opencv_frame.png'
+    
+    pic = cv2.imread(os.path.join(test_path, img_name), cv2.IMREAD_GRAYSCALE)
+    pic.resize((200,200))
+    pic = img_to_array(pic)
+    pic = np.array(pic)
+    pic = pic.reshape(-1,200,200,1)
+    print(pic.shape)
+    
+    classes = []
+    datasetFile = 'C:/Users/Rhyan Shah/Documents/GitHub/cosmosfinal/Facial_Recognition/Datasets'
+    for dir in os.scandir(datasetFile):
+        if dir.is_dir():
+            classes.append(dir)
+    
+    classes = np.array(classes)
+    pic_guess = model_cnn.predict(pic)
+    pic_single = classes[np.argmax(pic_guess, axis = -1)]
+    print(pic_single)
+    
+    os.remove(os.path.join(test_path,img_name))
+    
+    with open('C:/Users/Rhyan Shah//Documents/GitHub/cosmosfinal/Facial_Recognition/accuracy.txt', 'a') as fhand:
+        check = input('Was this recognition correct? (y/n) ')
+        if check == 'y':
+            fhand.write('Right\n')
+        elif check == 'n':
+            fhand.write('Wrong\n')
+        fhand.close()
+    
+    with open('C:/Users/Rhyan Shah//Documents/GitHub/cosmosfinal/Facial_Recognition/accuracy.txt', 'r') as fhand:
+        num = 0
+        den = 0
+        for line in fhand:
+            line.split()
+            if 'Right' in line:
+                num += 1
+                den += 1
+            elif 'Wrong' in line:
+                den +=1 
+                
+        perc_acc = (num/den)*100
+        print('Your feedback is valued, thank you for contributing to the data')
+        print('Currently, the algorithm is', perc_acc, 'percent accurate.')
